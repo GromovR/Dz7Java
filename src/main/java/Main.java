4ba8f2a6c8878e31d99a2d9a3929c9088fd05c1c
Gromov Roman <romzes.rz@gmail.com>
2023-08-15 20:28:04 +0300

    correct

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,12 +2,12 @@ import ru.netology.javaqa.javaqamvn.servises.CalcService;
 
 public class Main {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         CalcService service = new CalcService();
 
         int calculate = service.calculate(100000, 60000, 150000);
 
-        System.out.println(calculate  + " месяца(ев) отдыха");
+        System.out.println(calculate + " месяца(ев) отдыха");
 
     }
 }
--- src/main/java/ru/netology/javaqa/javaqamvn/servises/CalcService.java
+++ src/main/java/ru/netology/javaqa/javaqamvn/servises/CalcService.java
@@ -1,4 +1,5 @@
 package ru.netology.javaqa.javaqamvn.servises;
+
 public class CalcService {
     public int calculate(int income, int expenses, int threshold) {
         int count = 0;
@@ -9,11 +10,11 @@ public class CalcService {
             if (money >= threshold) {
                 count++;
                 money = (money - expenses) / 3;
-               // money = money / 3;
+                // money = money / 3;
 
             } else {
                 money = money + income - expenses;
-               // money = money - expenses;
+                // money = money - expenses;
             }
         }
         return count;
